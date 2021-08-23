d37b79a40088bce7e7b69ecf67b1798bd9e202df
Arnaux Stevan <arnauxx@hotmail.com>
2021-08-23 19:02:54 -0300

    Inicio projeto teste

--- CursoCSharpe/Program.cs
+++ CursoCSharpe/Program.cs
@@ -6,7 +6,7 @@ namespace CursoCSharpe
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("teste");
         }
     }
 }
