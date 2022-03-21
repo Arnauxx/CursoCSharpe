1d3f01be92e6af219829839da8a0e35d2547d56e
Arnaux Stevan <arnauxx@hotmail.com>
2022-03-21 18:29:36 -0300

    Troca de versão FINAL

--- IIComparable/Entities/Employee.cs
+++ IIComparable/Entities/Employee.cs
@@ -1,12 +1,34 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Globalization;
 
 namespace IIComparable.Entities
 {
-    class Employee
+    class Employee : IComparable
     {
+        public string Name { get; set; }
+        public double Salary { get; set; }
+
+        public Employee(string csvEmployee)
+        {
+            string[] vect = csvEmployee.Split(',');
+            Name = vect[0];
+            Salary = double.Parse(vect[1], CultureInfo.InvariantCulture);
+        }
+
+        public override string ToString()
+        {
+            return Name + ", " + Salary.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+        public int CompareTo(object obj)
+        {
+            if (!(obj is Employee))
+            {
+                throw new ArgumentException("Comparing error: argument is not an Employee");
+            }
+            Employee other = obj as Employee;
+            return Name.CompareTo(other.Name);
+        }
+
     }
 }
