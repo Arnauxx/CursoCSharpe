bcb603a1fe711282f80f0b008451a2e1950d476c
Arnaux Stevan <arnauxx@hotmail.com>
2022-03-22 18:36:10 -0300

    GitHashCode e Equals

---
+++ GetHashCodeAndEquals/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using GetHashCodeAndEquals.Entities;
+
+namespace GetHashCodeAndEquals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client a = new Client { Name = "Maria", Email = "maria@gmail.com" };
+            Client b = new Client { Name = "Alex", Email = "maria@gmail.com" };
+
+
+            Console.WriteLine(a.Equals(b));
+            Console.WriteLine(a == b);
+            Console.WriteLine(a.GetHashCode());
+            Console.WriteLine(b.GetHashCode());
+
+
+        }
+    }
+}
