3f36dd1d69df4bf78fdc5461fd6afcf206f7afc3
Arnaux Stevan <arnauxx@hotmail.com>
2022-03-22 17:33:10 -0300

    Generics parte 2

--- GenericsCSharp/PrintService.cs
+++ GenericsCSharp/PrintService.cs
@@ -2,12 +2,12 @@
 
 namespace GenericsCSharp
 {
-    class PrintService
+    class PrintService<T>
     {
-        private int[] _values = new int[10];
+        private T[] _values = new T[10];
         private int _count = 0;
 
-        public void AddValue(int value)
+        public void AddValue(T value)
         {
             if(_count == 10)
             {
@@ -17,7 +17,7 @@ namespace GenericsCSharp
             _count++;
         }
 
-        public int First()
+        public T First()
         {
             if (_count == 0)
             {
--- GenericsCSharp/Program.cs
+++ GenericsCSharp/Program.cs
@@ -6,13 +6,13 @@ namespace GenericsCSharp
     {
         static void Main(string[] args)
         {
-            PrintService printService = new PrintService();
+            PrintService<string> printService = new PrintService<string>();
             Console.Write("How many values? ");
             int n = int.Parse(Console.ReadLine());
 
             for(int i = 0; i < n; i++)
             {
-                int x = int.Parse(Console.ReadLine());
+                string x = (Console.ReadLine());
                 printService.AddValue(x);
             }
 
