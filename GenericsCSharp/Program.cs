220e203b38a74cc07ba3033006877facc6160c36
Arnaux Stevan <arnauxx@hotmail.com>
2022-03-22 17:18:15 -0300

    Generics parte 1

---
+++ GenericsCSharp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace GenericsCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintService printService = new PrintService();
+            Console.Write("How many values? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i < n; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+                printService.AddValue(x);
+            }
+
+            printService.Print();
+            Console.WriteLine("First: " + printService.First());
+        }
+    }
+}
