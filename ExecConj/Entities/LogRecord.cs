057621be17d1728c6451667bdb21732ba12b1277
Arnaux Stevan <stevan@link2go.com.br>
2022-06-13 16:13:46 -0300

    Exerc conjuntos

---
+++ ExecConj/Entities/LogRecord.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExecConj.Entities
+{
+    class LogRecord
+    {
+        public string UserName { get; set; }
+        public DateTime Instant { get; set; }
+
+        public override int GetHashCode()
+        {
+            return UserName.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if(!(obj is LogRecord))
+            {
+                return false;
+            }
+            LogRecord other = obj as LogRecord;
+            return UserName.Equals(other.UserName);
+        }
+
+
+    }
+}
---
+++ ExecConj/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using ExecConj.Entities;
+
+namespace ExecConj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<LogRecord> set = new HashSet<LogRecord>();
+
+            Console.Write("Enter file full path: ");
+            string path = Console.ReadLine();
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string[] line = sr.ReadLine().Split(' ');
+                        string name = line[0];
+                        DateTime instant = DateTime.Parse(line[1]);
+                        set.Add(new LogRecord { UserName = name, Instant = instant });
+                    }
+                }
+
+                Console.WriteLine("Total users: " + set.Count);
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
