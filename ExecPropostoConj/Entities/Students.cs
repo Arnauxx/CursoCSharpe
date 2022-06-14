f17f1030ec2648d48bfaada49ea7d0b6ab4120d6
Arnaux Stevan <stevan@link2go.com.br>
2022-06-14 10:53:36 -0300

    Solucao exercicio proposto

---
+++ ExecPropostoConj/Entities/Students.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExecPropostoConj.Entities
+{
+    internal class Students
+    {
+        public int Registration { get; set; }
+
+        public Students(int registration)
+        {
+            Registration = registration;
+        }
+
+        public override int GetHashCode()
+        {
+            return Registration.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Students))
+            {
+                return false;
+            }
+            return Registration.Equals(((Students)obj).Registration);
+        }
+    }
+}
---
+++ ExecPropostoConj/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using ExecPropostoConj.Entities;
+
+namespace ExecPropostoConj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            HashSet<Students> student = new HashSet<Students>();
+
+            Console.Write("How many students for course A ? ");
+            int a = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i < a; i++)
+            {
+                student.Add(new Students(int.Parse(Console.ReadLine())));
+            }
+
+            Console.Write("How many students for course B ? ");
+            int b = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < b; i++)
+            {
+                student.Add(new Students(int.Parse(Console.ReadLine())));
+            }
+            Console.Write("How many students for course C ? ");
+            int c = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < c; i++)
+            {
+                student.Add(new Students(int.Parse(Console.ReadLine())));
+            }
+
+            Console.WriteLine($"Total students: {student.Count}");
+
+        }
+    }
+}
