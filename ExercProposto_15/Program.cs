d12dcfb56a94f7abb8e993240a0d0df77c2ab2e0
Arnaux Stevan <stevan@link2go.com.br>
2022-06-14 14:28:27 -0300

    End of Generics Set and Dictionary

---
+++ ExercProposto_15/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+
+namespace ExercProposto_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter file full path: ");
+            string path = Console.ReadLine();
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    Dictionary<string, int> dictionary = new Dictionary<string, int>();
+                    while (!sr.EndOfStream)
+                    {
+                        string[] votingRecord = sr.ReadLine().Split(",");
+                        string candidate = votingRecord[0];
+                        int votes = int.Parse(votingRecord[1]);
+
+                        if (dictionary.ContainsKey(candidate))
+                        {
+                            dictionary[candidate] += votes;
+                        }
+                        else
+                        {
+                            dictionary[candidate] = votes;
+                        }
+                    }
+
+                    foreach (var item in dictionary)
+                    {
+                        Console.WriteLine($"{item.Key}: {item.Value}");
+                    }
+                }
+
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+
+        }
+    }
+}
