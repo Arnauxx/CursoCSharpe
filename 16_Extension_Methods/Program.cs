bcd36447a2ae4a143e0c0a9b3212297e8703281a
wesley <wesley@link2go.com.br>
2022-07-01 10:26:08 -0300

    Extensions methods

---
+++ 16_Extension_Methods/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _16_Extension_Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = new DateTime(2018, 7, 1, 8, 0, 00);
+            Console.WriteLine(dt.ElapsedTime());
+
+            Console.WriteLine();
+
+            String s1 = "Good morning dear students!";
+            Console.WriteLine(s1.Cut(10));
+
+            
+        }
+    }
+}
