bd0e5ecd291a625c94bb0c4f9e09582468108a4a
wesley <wesley@link2go.com.br>
2022-07-27 12:22:40 -0300

    Func_Select

---
+++ Func_Select/Entities/Products.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Func_Select.Entities
+{
+    class Products
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Products(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+
+        public override string ToString()
+        {
+            return Name + ", " + Price.ToString("F2");
+        }
+    }
+}
---
+++ Func_Select/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Func_Select.Entities;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Func_Select
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Products> list = new List<Products>();
+
+            list.Add(new Products("Tv", 900.00));
+            list.Add(new Products("Mouse", 50.00));
+            list.Add(new Products("Tablet", 350.50));
+            list.Add(new Products("HD Case", 80.90));
+
+            //Func<Products, string> func = p => p.Name.ToUpper();
+
+            List<string> result = list.Select(p => p.Name.ToUpper()).ToList();
+
+           
+            foreach(string p in result)
+            {
+                Console.WriteLine(p);
+            }
+                
+
+        }
+    }
+}
