ecf98f83f0f1a4ee34eab2e30fef85f83db33ed4
wesley <wesley@link2go.com.br>
2022-07-27 09:38:10 -0300

    Predicate

---
+++ Predicate/Entities/Products.cs
@@ -0,0 +1,22 @@
+﻿namespace Predicate.Entities
+{
+    class Products
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Products(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+
+        public override string ToString()
+        {
+            return Name + ", " + Price.ToString("F2");
+        }
+
+
+    }
+}
---
+++ Predicate/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Predicate.Entities;
+using System.Collections.Generic;
+using System.Globalization;
+        
+namespace Predicate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Products> list = new List<Products>();
+
+            list.Add(new Products("Tv", 900.00));
+            list.Add(new Products("Mouse", 50.00));
+            list.Add(new Products("Tablet", 350.50));
+            list.Add(new Products("HD Case", 80.90));
+
+            list.RemoveAll(ProductTest);
+
+            foreach(Products p in list)
+            {
+                Console.WriteLine(p);
+            }
+
+        }
+
+        public static bool ProductTest(Products p)
+        {
+            return p.Price >= 100;
+        }
+    }
+}
