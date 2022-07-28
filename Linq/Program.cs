20747e8d6882a12e93958007de16bdd1b639465e
wesley <wesley@link2go.com.br>
2022-07-28 10:33:48 -0300

    Linq

---
+++ Linq/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Linq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Specify the data source
+
+            int[] numbers = new int[] { 1, 2, 3, 4, 5 };
+
+            //Define the query
+            IEnumerable<int> result = numbers
+                .Where(x => x % 2 == 0)
+                .Select(x => x * 10);
+
+            //Execute the query
+            foreach(int x in result)
+            {
+                Console.WriteLine(x);
+            }
+
+        }
+    }
+}
