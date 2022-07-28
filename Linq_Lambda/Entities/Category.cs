0e8438d0887f7b36172549a0e3e5456962456cf0
Arnaux Stevan <arnauxx@hotmail.com>
2022-07-28 13:17:06 -0300

    Linq com lambda 1

---
+++ Linq_Lambda/Entities/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Linq_Lambda.Entities
+{
+    class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Tier { get; set; }
+
+
+    }
+}
---
+++ Linq_Lambda/Entities/Product.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace Linq_Lambda.Entities
+{
+    class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public Category Category { get; set; }
+
+
+        public override string ToString()
+        {
+            return Id
+                + ", "
+                + Name
+                + ", "
+                + Price.ToString("F2", CultureInfo.InvariantCulture)
+                + ", "
+                + Category.Name
+                +", "
+                + Category.Tier;
+        }
+
+
+
+    }
+}
