55f7c75bb90f63f4ddbd86b3676da3b0158d5404
Arnaux Stevan <arnauxx@hotmail.com>
2022-07-29 11:39:44 -0300

    Exec 17

---
+++ Exec_17/Entites/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exec_17.Entites
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+
+
+
+    }
+}
